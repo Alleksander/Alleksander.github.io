b9ba6ea3b86be2f7f0f5210eb95c23c9e48ec1f1
Alexander <aleksander1990@rambler.ru>
2022-06-03 13:28:11 +0300

    correction

--- pulse/js/script.js
+++ pulse/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.png"></button>',
         responsive: [
